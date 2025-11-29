806d6e4dca5ec9280617610ad42873babb2fdfd7
mirasko203 <mirasko203@gmail.com>
2025-11-30 03:07:55 +0500

    test.py

--- test.py
+++ test.py
@@ -131,12 +131,8 @@ flask_thread = threading.Thread(target=run_flask)
 flask_thread.daemon = True
 flask_thread.start()
 
-home()
-run_flask()
 
-bot_thread = threading.Thread(target=bot.infinity_polling)
-bot_thread.daemon = True
-bot_thread.start()
 bot.infinity_polling()
+
 
 
