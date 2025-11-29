92e80a1467c8e5ed013891d73366e7e743611dbc
mirasko203 <mirasko203@gmail.com>
2025-11-29 23:50:37 +0500

    test.py

--- test.py
+++ test.py
@@ -126,9 +126,8 @@ app = Flask(__name__)
 def home():
     return "Bot is alive!"
 
-# функция для запуска Flask
 def run_flask():
-    port = int(os.environ.get("PORT", 10000))  # Render автоматически задаёт PORT
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
 
 threading.Thread(target=run_flask, daemon=True).start()
